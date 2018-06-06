f9a4f7d4c50e1c9e1a466b28a3b898dca29c75df
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-06-06 08:44:25 -0700

    fixed draw card function

--- collector.js
+++ collector.js
@@ -1,4 +1,23 @@
-// global variables
+// check and see which of the 7 spots have players by checking bets(chips)
+// deal one card to each spot with chips with card face up
+// dealer deals 1 card face up to itself
+// deal another round of cards face up
+// dealer deal another card to itself face down
+// go to each player starting from left to right
+// if player says hit deal another card to the player
+// if card numbers are greater than or equal to 22 take cards
+// if player says stand, then move to next player
+// if all players have said stand or have no cards then dealer shows second card
+// if dealers hand is 17 or greater then stay, else draw a card
+// if dealers hand is greater than players hand and less than 22 then dealer wins
+// else dealer loses and player wins
+
+//deal randomly give cards to player/dealer from a deck
+//bet  place money on table       √√√
+//hit / draw          draw a new card from a deck and add it to current hand value   √√√
+//stand     stop current player turn and go to next
+//winner    evaluation on who is closest to 21
+
 var dealer;
 var player1 = true;
 var player2 = false;
@@ -236,105 +255,144 @@ var cardsArray = [
   {
     rank: 4,
     suit: "spades",
-    cardImage: "images/4S.png",
+    cardImage: "images/king-of-hearts.png",
   },
   {
     rank: 5,
     suit: "spades",
-    cardImage: "images/5S.png",
+    cardImage: "images/king-of-diamonds.png",
   },
   {
     rank: 6,
     suit: "spades",
-    cardImage: "images/6S.png",
+    cardImage: "images/queen-of-hearts.png",
   },
   {
     rank: 7,
     suit: "spades",
-    cardImage: "images/7S.png",
+    cardImage: "images/queen-of-diamonds.png",
   },
   {
     rank: 8,
     suit: "spades",
-    cardImage: "images/8S.png",
+    cardImage: "images/king-of-hearts.png",
   },
   {
     rank: 9,
     suit: "spades",
-    cardImage: "images/9S.png",
+    cardImage: "images/king-of-diamonds.png",
   },
   {
     rank: 10,
     suit: "spades",
-    cardImage: "images/10S.png",
+    cardImage: "images/queen-of-hearts.png",
   },
   {
     rank: 10,
     suit: "spades",
-    cardImage: "images/JS.png",
+    cardImage: "images/queen-of-diamonds.png",
   },
   {
     rank: 10,
     suit: "spades",
-    cardImage: "images/QS.png",
+    cardImage: "images/king-of-hearts.png",
   },
   {
     rank: 10,
     suit: "spades",
-    cardImage: "images/KS.png",
+    cardImage: "images/king-of-diamonds.png",
   },
 ];
 
-//deal randomly give cards to player/dealer from a deck
-function deal() {
+// find out how many players
+document.getElementById('submit').addEventListener('click', function() {
+  numberOfPlayers = document.getElementById('playerCount').value;
+  console.log(numberOfPlayers);
+  return numberOfPlayers;
+});
+
+// deal first card to player
+var deal = function() {
   if(bet1.textContent !== '' && bet1.textContent >= 5) {
     var playerCard1 = document.createElement('img');
-    playerCard1.setAttribute('src', drawCard());
-    playerCard1.setAttribute('class', 'image');
+    playerCard1.src = "images/aceOfSpades.jpg";
+    playerCard1.classList.add('image');
     firstHand.appendChild(playerCard1);
     var dealerCard1 = document.createElement('img');
-    dealerCard1.setAttribute('src', drawCard())
-    dealerCard1.setAttribute('class', 'image');
+    dealerCard1.src = "images/twoOfSpades.png";
+    dealerCard1.classList.add('image');
     dealerHand.appendChild(dealerCard1);
     var playerCard2 = document.createElement('img');
-    playerCard2.setAttribute('src', drawCard());
+    playerCard2.src = "images/threeOfSpades.png";
     playerCard2.classList.add('imageCard2');
     firstHand.appendChild(playerCard2);
     var dealerCard2 = document.createElement('img');
-    dealerCard2.setAttribute('src', drawCard());
+    dealerCard2.src = "images/cardback.png";
     dealerCard2.classList.add('imageDealerCard2');
     dealerHand.appendChild(dealerCard2);
   }
 }
 document.getElementById('deal').addEventListener("click", deal);
 
-//bet  place money on table
-document.getElementById('bet').addEventListener('click', function() {
-  playerBet = document.getElementById('amount').value;
-  if (player1 === true) {
-    document.getElementsByClassName('bet1')[0].textContent = playerBet;
-  }
+
+// player hits function
+document.getElementById('hit').addEventListener('click', drawCard);
+
+function removeElement(elementClassName) {
+  console.log('working');
+  // Removes an element from the document
+  var element = document.getElementsByClassName(elementClassName)[0];
+  element.parentNode.removeChild(element);
+}
+
+// flip dealers facedown card
+document.getElementById('stand').addEventListener('click', function() {
+
+  var faceDownCard = document.getElementsByClassName('imageDealerCard2')[0];
+  removeElement('imageDealerCard2');
+  var dealerFaceUpCard2 = document.createElement('img');
+  dealerFaceUpCard2.src = cardDeck['30'];
+  dealerFaceUpCard2.classList.add('imageDealerFaceUpCard2');
+  dealerHand.appendChild(dealerFaceUpCard2);
+  dealerProcess();
 });
 
+// dealer process
+    // get random number 1 -52
+    // get card w/ random number
+    // assign to player/dealer
+    // repeat if needed up to four
+    // check to see if the dealer has 21 or not
+var dealerProcess = function() {
+  // find out current hand value
+  drawCard();
+  // push drawn card into array
+  var variable = drawCard();
+  console.log(variable);
+  // push 2nd drawn card into array
+  // combine cards rank in array and see if it's 17 or less
+ // cardsArray[i].rank
+ var placeholder;
+  var dealerCards = placeholder;
+  while(dealerCard < 17) {
+    // draw a card
+    var value = placeholder;// drawCard function
+    // add new card value to original dealerCards
+  }
+    //determine winner
+}
 
-//hit / draw          draw a new card from a deck and add it to current hand value
 // This function draws a random card for anyone
 var drawCard = function() {
   var cardElement = document.createElement('img');
   var randomCard = function() {
-    return cardsArray[Math.floor(Math.random() * 52)].cardImage;
+    return cardsArray[Math.floor(Math.random * 51)];
   }
   cardElement.setAttribute('src', randomCard());
-  cardElement.setAttribute('class', 'image');
   document.getElementById('dealer').appendChild(cardElement);
 }
 
-//stand     stop current player turn and go to next
-function stand() {
-
-}
-
-//winner    evaluation on who is closest to 21
-function winner() {
-
-}
+// when you click place bet button place bet amount in bet div and switch player
+document.getElementById('bet').addEventListener('click', function() {
+  playerBet = document.getElementById('amount').value;
+});
